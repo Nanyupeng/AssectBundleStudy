909e726abd9cd3cc629378612a1ebb3a8183b9a6
Nan Yupeng <38316241+Nanyupeng@users.noreply.github.com>
2020-03-08 19:36:55 +0800

    Assetbundle定义与解析
    2020_3_8
    Assetbundle定义
    Assetbundle打包构建
    Assetbundle本地ab包三种读取方式

---
+++ AssectBundleStudy/Assets/Editor/CreatAssetBundles.cs
@@ -0,0 +1,13 @@
+﻿using UnityEditor;
+using System.IO;
+public class CreatAssetBundles 
+{
+    [MenuItem("Assets/Build AssetBundles")]
+   static void BuildALLAssetBundles()
+    {
+        string dr = "AssetBundles";
+        if (!Directory.Exists(dr))
+            Directory.CreateDirectory(dr);
+        BuildPipeline.BuildAssetBundles(dr, BuildAssetBundleOptions.None,BuildTarget.StandaloneWindows64);
+    }
+}
---
+++ AssectBundleStudy/Assets/Scripts/LoadFromFileExample.cs
@@ -0,0 +1,46 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System.IO;
+public class LoadFromFileExample : MonoBehaviour
+{
+    string path = "AssetBundles/cubewall.nan";
+    IEnumerator Start()
+    {
+        //AssetBundle ab = AssetBundle.LoadFromFile("AssetBundles/share.nan");
+        //AssetBundle ab1 = AssetBundle.LoadFromFile("AssetBundles/cubewall.nan");
+        //GameObject go = ab1.LoadAsset<GameObject>("CubeWall");
+        //Instantiate(go);
+
+
+        //第一种AssetBundle加载方式   AssetBundle.LoadFromMemory
+        //AssetBundle ab = AssetBundle.LoadFromMemory(File.ReadAllBytes(path));
+        //GameObject go = ab.LoadAsset<GameObject>("CubeWall");
+        //Instantiate(go);
+
+        //第二种AssetBunbdle加载方式  AssetBundle.LoadFromFileAsync
+        //AssetBundleCreateRequest assetBundleCreate = AssetBundle.LoadFromFileAsync(path);
+        //AssetBundle ab = assetBundleCreate.assetBundle;
+        //GameObject go = ab.LoadAsset<GameObject>("CubeWall");
+        //Instantiate(go);
+
+        //第三种AssetBundle加载方式  WWW.LoadFromCacheOrDownload
+        while (Caching.ready == false)
+            yield return null;
+
+        WWW ww = WWW.LoadFromCacheOrDownload(@"F:\NanProject\AssectBundleStudy\AssectBundleStudy\AssectBundleStudy\AssetBundles\cubewall.nan", 1);
+        yield return ww;
+        if (ww.error != null)
+        {
+            Debug.Log(ww.error);
+            yield break;
+        }
+        if (ww.isDone)
+        {
+            AssetBundle ab = ww.assetBundle;
+            GameObject go = ab.LoadAsset<GameObject>("CubeWall");
+            Instantiate(go);
+        }
+
+    }
+}
